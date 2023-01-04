2f190ac28def7b868c637f78ca42a88e5c4cd1c9
Upasanaraghav01 <119447299+Upasanaraghav01@users.noreply.github.com>
2023-01-04 18:11:47 +0530

    pattern2 done

---
+++ pattern2.cpp
@@ -0,0 +1,26 @@
+//pattern   *
+         //**
+        //***
+       //****  
+#include<iostream>
+using namespace std;
+int main()
+{
+  int row,col;
+  cin>>row>>col;
+  for(int i=1;i<row;i++)
+    {
+      for(int j=1;j<col;j++)
+        {
+          if(j<col-i)
+          {
+          cout<<" ";
+            }
+          else{
+            cout<<"*";
+          }
+        }
+      cout<<endl;
+    }
+  return 0;
+  }
